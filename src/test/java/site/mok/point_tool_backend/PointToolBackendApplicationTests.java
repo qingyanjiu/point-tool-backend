39859e19ee4f4c2f11777b430ed59bfe38822caa
Louis <louis@LouismatoMacBook-Air.local>
2024-08-03 16:48:06 +0800

    init

---
+++ src/test/java/site/mok/point_tool_backend/PointToolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package site.mok.point_tool_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointToolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
